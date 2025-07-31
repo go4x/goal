a7032a3416f8a756aedbdb8f9b3cb04c3a0b8c67
gopher <gophero@proton.me>
2025-07-31 03:59:18 +0000

    test: add test cases for assert

--- assert/assert.go
+++ assert/assert.go
@@ -8,6 +8,10 @@ import (
 
 // basic methods
 
+// Require is the basic method to assert a condition, if the condition is false, it will panic with the given format and arguments
+// format is a string that will be formatted with the arguments
+// v is a variadic parameter that will be used to format the string
+// if the condition is true, the function will return normally
 func Require(cond bool, format string, v ...any) {
 	if !cond {
 		panic(fmt.Sprintf(format, v...))
@@ -16,28 +20,35 @@ func Require(cond bool, format string, v ...any) {
 
 // convenient methods
 
+// True asserts that the given boolean value is true
 func True(b bool) {
 	Require(b, "assert failed, expects [%t] but found [%t]", true, b)
 }
 
+// Nil asserts that the given value is nil
 func Nil(t any) {
 	Require(t == nil, "assert failed, expects [nil] but found [not nil]: %v", t)
 }
 
+// NoneNil asserts that the given value is not nil
 func NoneNil(t any) {
 	Require(t != nil, "assert failed, expects [not nil] but found [nil]")
 }
 
+// Blank asserts that the given string is blank (empty or only contains whitespace).
 func Blank(s string) {
 	s = strings.TrimSpace(s)
 	Require(s == "", "assert failed, expects [\"\"] but found [%s]", s)
 }
 
+// NotBlank asserts that the given string is not blank (not empty or not only contains whitespace).
 func NotBlank(s string) {
 	s = strings.TrimSpace(s)
 	Require(s != "", "assert failed, expects [not empty] but found [\"\"]")
 }
 
+// HasElems asserts that the given collection is not nil and has elements. Param c should be a collection type such as array, map, slice, or channel,
+// do nothing if not.
 func HasElems(c any) {
 	typ := reflect.TypeOf(c).Kind()
 	if typ == reflect.Array || typ == reflect.Map || typ == reflect.Slice || typ == reflect.Chan {
@@ -46,10 +57,12 @@ func HasElems(c any) {
 	}
 }
 
+// Equals asserts that the given two values are equal
 func Equals(t1 any, t2 any) {
 	Require(t1 == t2, "assert failed, expecting t1 equals t2 but not")
 }
 
+// DeepEquals asserts that the given two values are deeply equal, it will use reflect.DeepEqual to compare the two values.
 func DeepEquals(t1 any, t2 any) {
 	Require(reflect.DeepEqual(t1, t2), "assert failed, expects %v deep equals %v, but not", t1, t2)
 }
--- assert/assert_test.go
+++ assert/assert_test.go
@@ -38,3 +38,257 @@ func TestRequire(t *testing.T) {
 		assert.Require(false, "not match the condition")
 	}()
 }
+func TestTrue(t *testing.T) {
+	logger := got.Wrap(t)
+	logger.Title("test assert.True method")
+
+	logger.Case("give true, should not panic")
+	func() {
+		defer func() {
+			if err := recover(); err != nil {
+				logger.Fail("should not panic, but got: %v", err)
+			} else {
+				logger.Pass("should not panic")
+			}
+		}()
+		assert.True(true)
+	}()
+
+	logger.Case("give false, should panic")
+	func() {
+		defer func() {
+			if err := recover(); err == nil {
+				logger.Fail("should panic, but not")
+			} else {
+				logger.Pass("should panic: %v", err)
+			}
+		}()
+		assert.True(false)
+	}()
+}
+
+func TestNil(t *testing.T) {
+	logger := got.Wrap(t)
+	logger.Title("test assert.Nil method")
+
+	logger.Case("give nil, should not panic")
+	func() {
+		defer func() {
+			if err := recover(); err != nil {
+				logger.Fail("should not panic, but got: %v", err)
+			} else {
+				logger.Pass("should not panic")
+			}
+		}()
+		assert.Nil(nil)
+	}()
+
+	logger.Case("give not nil, should panic")
+	func() {
+		defer func() {
+			if err := recover(); err == nil {
+				logger.Fail("should panic, but not")
+			} else {
+				logger.Pass("should panic: %v", err)
+			}
+		}()
+		assert.Nil(1)
+	}()
+}
+
+func TestNoneNil(t *testing.T) {
+	logger := got.Wrap(t)
+	logger.Title("test assert.NoneNil method")
+
+	logger.Case("give not nil, should not panic")
+	func() {
+		defer func() {
+			if err := recover(); err != nil {
+				logger.Fail("should not panic, but got: %v", err)
+			} else {
+				logger.Pass("should not panic")
+			}
+		}()
+		assert.NoneNil(1)
+	}()
+
+	logger.Case("give nil, should panic")
+	func() {
+		defer func() {
+			if err := recover(); err == nil {
+				logger.Fail("should panic, but not")
+			} else {
+				logger.Pass("should panic: %v", err)
+			}
+		}()
+		assert.NoneNil(nil)
+	}()
+}
+
+func TestBlank(t *testing.T) {
+	logger := got.Wrap(t)
+	logger.Title("test assert.Blank method")
+
+	logger.Case("give blank string, should not panic")
+	func() {
+		defer func() {
+			if err := recover(); err != nil {
+				logger.Fail("should not panic, but got: %v", err)
+			} else {
+				logger.Pass("should not panic")
+			}
+		}()
+		assert.Blank("")
+		assert.Blank("   ")
+		assert.Blank("\n\t")
+	}()
+
+	logger.Case("give not blank string, should panic")
+	func() {
+		defer func() {
+			if err := recover(); err == nil {
+				logger.Fail("should panic, but not")
+			} else {
+				logger.Pass("should panic: %v", err)
+			}
+		}()
+		assert.Blank("abc")
+	}()
+}
+func TestNotBlank(t *testing.T) {
+	logger := got.Wrap(t)
+	logger.Title("test assert.NotBlank method")
+
+	logger.Case("give not blank string, should not panic")
+	func() {
+		defer func() {
+			if err := recover(); err != nil {
+				logger.Fail("should not panic, but got: %v", err)
+			} else {
+				logger.Pass("should not panic")
+			}
+		}()
+		assert.NotBlank("abc")
+		assert.NotBlank("  abc  ")
+		assert.NotBlank("\nabc\t")
+	}()
+
+	logger.Case("give blank string, should panic")
+	func() {
+		defer func() {
+			if err := recover(); err == nil {
+				logger.Fail("should panic, but not")
+			} else {
+				logger.Pass("should panic: %v", err)
+			}
+		}()
+		assert.NotBlank("")
+	}()
+}
+
+func TestHasElems(t *testing.T) {
+	logger := got.Wrap(t)
+	logger.Title("test assert.HasElems method")
+
+	logger.Case("give non-empty slice, should not panic")
+	func() {
+		defer func() {
+			if err := recover(); err != nil {
+				logger.Fail("should not panic, but got: %v", err)
+			} else {
+				logger.Pass("should not panic")
+			}
+		}()
+		assert.HasElems([]int{1, 2, 3})
+		assert.HasElems([2]int{1, 2})
+		assert.HasElems(map[string]int{"a": 1})
+		ch := make(chan int, 1)
+		ch <- 1
+		assert.HasElems(ch)
+	}()
+
+	logger.Case("give empty slice, should panic")
+	func() {
+		defer func() {
+			if err := recover(); err == nil {
+				logger.Fail("should panic, but not")
+			} else {
+				logger.Pass("should panic: %v", err)
+			}
+		}()
+		assert.HasElems([]int{})
+	}()
+
+	logger.Case("give nil slice, should panic")
+	func() {
+		defer func() {
+			if err := recover(); err == nil {
+				logger.Fail("should panic, but not")
+			} else {
+				logger.Pass("should panic: %v", err)
+			}
+		}()
+		var s []int
+		assert.HasElems(s)
+	}()
+}
+
+func TestEquals(t *testing.T) {
+	logger := got.Wrap(t)
+	logger.Title("test assert.Equals method")
+
+	logger.Case("give equal values, should not panic")
+	func() {
+		defer func() {
+			if err := recover(); err != nil {
+				logger.Fail("should not panic, but got: %v", err)
+			} else {
+				logger.Pass("should not panic")
+			}
+		}()
+		assert.Equals(1, 1)
+		assert.Equals("abc", "abc")
+	}()
+
+	logger.Case("give not equal values, should panic")
+	func() {
+		defer func() {
+			if err := recover(); err == nil {
+				logger.Fail("should panic, but not")
+			} else {
+				logger.Pass("should panic: %v", err)
+			}
+		}()
+		assert.Equals(1, 2)
+	}()
+}
+
+func TestDeepEquals(t *testing.T) {
+	logger := got.Wrap(t)
+	logger.Title("test assert.DeepEquals method")
+
+	logger.Case("give deeply equal values, should not panic")
+	func() {
+		defer func() {
+			if err := recover(); err != nil {
+				logger.Fail("should not panic, but got: %v", err)
+			} else {
+				logger.Pass("should not panic")
+			}
+		}()
+		assert.DeepEquals([]int{1, 2}, []int{1, 2})
+		assert.DeepEquals(map[string]int{"a": 1}, map[string]int{"a": 1})
+	}()
+
+	logger.Case("give not deeply equal values, should panic")
+	func() {
+		defer func() {
+			if err := recover(); err == nil {
+				logger.Fail("should panic, but not")
+			} else {
+				logger.Pass("should panic: %v", err)
+			}
+		}()
+		assert.DeepEquals([]int{1, 2}, []int{2, 1})
+	}()
+}
